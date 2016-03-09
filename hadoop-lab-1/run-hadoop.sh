e29081580a8834c66e01ef336da00387bff035c5
Igor Lyubimov <stoneflash@gmail.com>
2016-03-09 13:07:14 +0300

    python special lab 1

--- hadoop-lab-1/run-hadoop.sh
+++ hadoop-lab-1/run-hadoop.sh
@@ -1,1 +1,2 @@
+hadoop fs -rmdir --ignore-fail-on-non-empty /users/adverts/
 hadoop jar /opt/cloudera/parcels/CDH/lib/hadoop-mapreduce/hadoop-streaming.jar -D mapred.reduce.tasks=1 -mapper 'python mapper.py' -reducer 'python reducer.py' -file '/home/ubuntu/mapper.py' -file '/home/ubuntu/reducer.py' -input /users/advert/advert.log -output /users/adverts
